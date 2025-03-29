040969d320cabcc9fa1b17ec8720395839e022e8
davelin18yufan <davelin30630@gmail.com>
2025-03-29 16:11:06 +0800

    [feat] update type definition

--- types/index.ts
+++ types/index.ts
@@ -1,18 +1,18 @@
 export interface Customer {
-  id: string;
-  phone: string;
-  balance: number;
-  name: string;
-  lastVisit: string;
-  transactionCount?: number;
-  totalSpent?: number;
-  latestNote?: { category: string; content: string } | null;
+  id: string
+  phone: string
+  balance: number
+  name: string
+  lastVisit: string
+  transactionCount?: number
+  totalSpent?: number
+  latestNote?: Note | null
 }
 
 export interface StoreLocation {
-  id: string;
-  name: string;
-  customers: Customer[];
+  id: string
+  name: string
+  customers: Customer[]
 }
 
 export interface Member {
@@ -23,4 +23,29 @@ export interface Member {
   lastBalanceUpdate: string
   birthday?: string
   gender?: "male" | "female" | "other"
+  latestNote?: Note | null
+}
+
+export interface Note {
+  noteId: string
+  memberId: string
+  category: string
+  content: string
+  createdAt: string 
+  updatedAt: string 
+}
+
+export interface FrequentMembers {
+  storeId: string
+  storeName: string
+  memberId: string
+  phone: string
+  name: string
+  balance: number | null
+  lastVisit: string | null
+  transactionCount: number
+  totalSpent: number
+  latestNoteCategory: string | null
+  latestNoteContent: string | null
+  latestNoteDate: string | null
 }
