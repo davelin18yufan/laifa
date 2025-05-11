e7ae512375ac9dd763119ae4bbdf9f8e0ae1653f
davelin18yufan <davelin30630@gmail.com>
2025-05-11 12:35:27 +0800

    [fix] fix protected middleware error

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,15 @@
 import { NextRequest, NextResponse } from "next/server"
 import { getToken } from "next-auth/jwt"
 
+const protedPath = ["/shopkeeper", "/admin", "/order"]
+
 export async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
   const { pathname } = req.nextUrl
 
   // 受保護路徑
-  const isProtectedPath =
-    pathname.startsWith("/shopkeeper") || pathname.startsWith("/admin")
-
+  const isProtectedPath = protedPath.some((path) => pathname.startsWith(path))
+  
   // 未登入且訪問受保護路徑，重定向到登入頁面並帶上 callbackUrl
   if (!token && isProtectedPath) {
     const loginUrl = new URL("/login", req.url)
@@ -21,7 +22,7 @@ export async function middleware(req: NextRequest) {
     return NextResponse.next()
   }
 
-  // Shopkeeper 只能訪問 /shopkeeper，不能訪問 /admin
+  // Shopkeeper不能訪問 /admin
   if (token?.role === "shopkeeper" && pathname.startsWith("/admin")) {
     return NextResponse.redirect(new URL("/login", req.url))
   }
@@ -30,5 +31,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/shopkeeper/:path*", "/admin/:path*", "/orders/:path*"], 
+  matcher: ["/shopkeeper/:path*", "/admin/:path*", "/order/:path*"], 
 }
